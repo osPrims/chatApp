e652f8834367d26433dde284f2b3c80f2ea394d5
osbins <arpitjain.2001@gmail.com>
2021-10-04 14:23:04 +0530

    configured port for Heroku deployment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = process.env.PORT || 8080
 
 // Load external styles and scripts from folder 'public'
 app.use(express.static("public"));
@@ -34,6 +35,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Listening on *:3000");
+server.listen(port, () => {
+  console.log("Listening on:", port);
 });
