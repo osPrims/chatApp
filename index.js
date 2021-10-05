c9a388d1aed25d57ac8300a5d4de2be21520e280
manthanabc <manthanmji@gmail.com>
2021-10-05 16:14:49 +0530

    added online list

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const port = process.env.PORT || 8080
-
+let users = []; 
 // Load external styles and scripts from folder 'public'
 app.use(express.static("public"));
 
@@ -15,14 +15,27 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
 io.on("connection", (socket) => {
   console.log("A user has connected");
-
-  socket.broadcast.emit("con", "A user has connected");
-
-  socket.on("chat message", (user, msg) => {
-    console.log(user + "(user): ", msg);
-    io.emit("chat message", user, msg);
+  socket.broadcast.emit("connected", socket.id);
+  //let = current_user = { name: "", id: socket.id };
+  socket.name ="";
+  let filtered_users = users.filter((user) => user.id == socket.id);
+  if(filtered_users != []) {
+    users.push({
+      name: "",
+      id : socket.id
+    });
+  }
+  socket.on("chat message", (user_name, msg) => {
+    console.log(user_name + "(user): ", msg);
+    socket.name = user_name;
+    io.emit("chat message", {name:socket.name, id:socket.id} , msg);
+    let current_user = users.filter((user) =>{ if(user.id == socket.id) {user.name = user_name} });
   });
 
   socket.on("typing", () => {
@@ -31,7 +44,8 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     console.log("A user has disconnected");
-    io.emit("con", "A user has disconnected");
+    users = users.filter((user) => user.id != socket.id);
+    io.emit("disconnected", socket.id);
   });
 });
 
--- public/client.js
+++ public/client.js
@@ -3,7 +3,13 @@ let form = document.getElementById("form");
 let input = document.getElementById("input");
 let feedback = document.getElementById("feedback");
 let username = document.getElementById("username");
-
+let users = [];
+fetch("/users")
+.then((user) => user.json())
+.then((data) => {
+  users = data;
+  users.forEach((user) => addusertolist(user));
+});
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -14,21 +20,64 @@ form.addEventListener("submit", (e) => {
   if (username.readOnly == false) {
     username.readOnly = true;
   }
+  console.log(users);
 });
 
 // Show 'A user has connected'
-socket.on("con", (msg) => {
+// socket.on("con", (msg) => {
+//   let item = document.createElement("li");
+//   item.style.color = "blue";
+//   item.textContent = msg;
+//   messages.appendChild(item);
+
+//   let result = await fetch("/users");
+//   users = await result.json();
+  //update online members
+  // setTimeout(async() => {
+  //   let item = document.createElement("li");
+  //   item.style.color = "blue";
+  //   item.textContent = users[0].name;
+  //   online.appendChild(item);
+  //   }, 1000);
+// });
+socket.on("connected", (id) => {
+  users.push({ name:"", id: id});
   let item = document.createElement("li");
   item.style.color = "blue";
-  item.textContent = msg;
+  item.textContent = "A user has connected";
   messages.appendChild(item);
+
+  addusertolist({ name: "anonimus", id: id});
+  // let user = document.createElement("li");
+  // user.style.color = "blue";
+  // user.textContent = "anonimus";
+  // user.id = id;
+  // online.appendChild(user);
+});
+
+socket.on("disconnected", (id) => {
+  users = users.filter((user) => user.id != id);
+  let item = document.createElement("li");
+  item.style.color = "blue";
+  item.textContent = "A user has disconnected";
+  messages.appendChild(item);
+
+  removeuserfromlist(id);
 });
 
 socket.on("chat message", (user, msg) => {
   let item = document.createElement("li");
-  item.innerHTML = `<b>${ user }: </b>` + msg;
+  item.innerHTML = `<b>${ user.name }: </b>` + msg;
   messages.appendChild(item);
   feedback.innerHTML = "";
+
+  users.forEach((saved_user) => {
+    if(saved_user.id == user.id) {
+      saved_user.name = user.name
+      let item = document.getElementById(user.id);
+      item.textContent = user.name;
+    }
+  });
 });
 
 input.addEventListener("keypress", () => {
@@ -38,3 +87,17 @@ input.addEventListener("keypress", () => {
 socket.on("typing", () => {
   feedback.innerHTML = "A user is typing...";
 });
+
+let addusertolist = (user) => {
+  let item = document.createElement("li");
+  item.style.color = "blue";
+  item.textContent = user.name;
+  item.id = user.id
+  online.appendChild(item);
+}
+
+let removeuserfromlist = (userid) => {
+  let item = document.getElementById(userid);
+  if(item != null)
+    item.remove();
+}
