f7e44f36a3a9fb2ba3da9d6451855f8e4d6d5ab2
cp-Coder <cplusplusCoder.vnit@gmail.com>
2021-12-11 19:18:53 +0530

    Rsolved the conflict

--- public/client.js
+++ public/client.js
@@ -7,9 +7,9 @@ let messages = document.getElementById('messages');
 let online = document.getElementById('online');
 let sendBtn = document.querySelector('.btn--send');
 
-let users = [];
-let selfId;
-let md;
+let users = []
+let selfId
+let md
 
 md = window.markdownit({
   html: false,
@@ -19,18 +19,19 @@ md = window.markdownit({
 
 
 // Color for the messages 
-let colors = ['#0080FF', '#8000FF', '#FF00FF', '#FF0080', '#FF0000', '#FF8000', '#80FF00', '#00FF00', '#00FF80'];
+let colors = ['#0080FF', '#8000FF', '#FF00FF', '#FF0080', '#FF0000', '#FF8000', '#80FF00', '#00FF00', '#00FF80']
+
 let coll = document.getElementsByClassName("collapsible");
 
 coll[0].addEventListener("click", function () {
   this.classList.toggle("active");
-  let content = this.nextElementSibling;
+  var content = this.nextElementSibling;
   if (content.style.display === "block") {
     content.style.display = "none";
   } else {
     content.style.display = "block";
   }
-});
+})
 
 
 // Fetch users online as soon as you connect
@@ -38,12 +39,13 @@ fetch("/users")
   .then((user) => user.json())
   .then((data) => {
     data.forEach((user) => {
-      user.color = colors[0];
-      colors = colors.splice(1);
-      addUserToList(user);
-    });
-    users = users.concat(data);
-  });
+      user.color = colors[0]
+      colors = colors.splice(1)
+      addusertolist(user)
+    })
+    users = users.concat(data)
+  })
+
 
 // Sent a chat message to server when submit a form
 form.addEventListener("submit", (e) => {
@@ -72,75 +74,76 @@ form.addEventListener("submit", (e) => {
 
 // Received from server when someone gets connected
 socket.on("connected", (id) => {
-  users.push({ name: "Anonymous", id: id, color: colors[0] });
-  addUserToList({ name: "Anonymous", id: id, color: colors[0] });
-  colors = colors.splice(1);
+
+  users.push({ name: "Anonymous", id: id, color: colors[0] })
+  addusertolist({ name: "Anonymous", id: id, color: colors[0] })
+  colors = colors.splice(1)
 
   if (selfId) {
     let item = document.createElement("li");
-    item.className = 'connection';
+    item.className = 'connection'
     item.style.color = "black";
     item.textContent = "A user has connected";
     item.style.backgroundColor = "LightGray";
     messages.appendChild(item);
   }
   else {
-    selfId = id;
-    feedback.innerHTML = "Welcome to Chat App - Instant Messaging App";
-    playSound('/welcome.mp3');
+    selfId = id
+    feedback.innerHTML = "Welcome to Chat App - Instant Messaging App"
+    playSound('/welcome.mp3')
   }
 
-  scrollSmoothToBottom('main');
+  scrollSmoothToBottom('main')
 });
 
 // Received from server when someone gets disconnected
 socket.on("disconnected", (id) => {
-  let current_user = users.filter((user) => user.id === id);
-  colors.push(current_user[0].color);
+  let current_user = users.filter((user) => user.id === id)
+  colors.push(current_user[0].color)
   users = users.filter((user) => user.id != id);
 
   let item = document.createElement("li");
   item.style.color = "black";
-  item.className = 'connection';
+  item.className = 'connection'
   item.style.backgroundColor = "LightGray";
-  item.textContent = current_user[0].name + ' has disconnected';
+  item.textContent = current_user[0].name + ' has disconnected'
   messages.appendChild(item);
 
-  scrollSmoothToBottom('main');
-  removeUserFromList(id);
-  feedback.innerHTML = '';
+  scrollSmoothToBottom('main')
+  removeuserfromlist(id);
+  feedback.innerHTML = ''
 });
 
-// Received from a server when a chat message is received
+// Recieved from a server when a chat message is received
 socket.on("chat message", (user, msg) => {
   let item = document.createElement("li");
-  item.className = user.id;
-  let current_user = users.filter((_user_) => _user_.id === user.id);
+  item.className = user.id
+  let current_user = users.filter((_user_) => _user_.id === user.id)
   if (selfId === user.id) {
-    item.classList.add('self');
+    item.classList.add('self')
   }
   else {
-    item.style.color = current_user[0].color;
+    item.style.color = current_user[0].color
   }
 
-  item.innerHTML = `<b>${user.name}&nbsp;</b><br>` + msg;
-  item.classList.add('messages');
-  messages.appendChild(item);
+  item.innerHTML = `<b>${user.name}&nbsp;</b><br>` + `<div class="userMsg">${md.render(msg)}</div>`;
+  // item.innerHTML = `<b>${ user.name }: </b>` + `<div class="userMsg">${msg}</div>`;
+  item.classList.add('messages')
+  messages.appendChild(item)
 
-
-  scrollSmoothToBottom('main');
-  if (user.id !== selfId) playSound('/notification.mp3');
+  scrollSmoothToBottom('main')
+  if (user.id !== selfId) playSound('/notification.mp3')
   feedback.innerHTML = "";
 
   // check if someone has set their name
   users.forEach((saved_user) => {
     if (saved_user.id === user.id) {
-      saved_user.name = user.name;
+      saved_user.name = user.name
       let item = document.getElementById(user.id);
       item.innerHTML = '<span class="dot"></span>' + user.name;
     }
   });
-});
+})
 
 // Sent to server when you type
 input.addEventListener("keypress", () => {
@@ -152,37 +155,36 @@ let fbTimer;
 socket.on("typing", (user) => {
   clearTimeout(fbTimer);
   feedback.innerHTML = user + " is typing...";
-  scrollSmoothToBottom('main');
   fbTimer = setTimeout(() => {
     feedback.innerHTML = "";
   }, 2000);
 });
 
 // Add user to collapsible
-let addUserToList = (user) => {
+let addusertolist = (user) => {
   let item = document.createElement("li");
   item.style.color = (selfId) ? user.color : 'blue';
   item.innerHTML = '<span class="dot"></span>' + user.name;
-  item.id = user.id;
-  item.onclick = handleOnlineClick.bind(null, user.id);
+  item.id = user.id
+  item.onclick = handleOnlineClick.bind(null, user.id)
   online.appendChild(item);
-};
+}
 
 // Remove use from collapsible
-let removeUserFromList = (userId) => {
-  let item = document.getElementById(userId);
+let removeuserfromlist = (userid) => {
+  let item = document.getElementById(userid);
   if (item != null)
     item.remove();
-};
+}
 
 // Auto scroll to bottom when messages come
 function scrollSmoothToBottom(id) {
-  var div = document.getElementById(id);
-  var scrollHeight = div.scrollHeight;
+  var div = document.getElementById(id)
+  var scrollHeight = div.scrollHeight
   div.scroll({
     top: scrollHeight + 10,
     behavior: "smooth"
-  });
+  })
 }
 
 function playSound(url) {
@@ -191,21 +193,35 @@ function playSound(url) {
 }
 
 function handleOnlineClick(id) {
-  let current_user = users.filter((user) => user.id === id);
-  input.value = `@${current_user[0].name}`;
+  let current_user = users.filter((user) => user.id === id)
+  input.value = `@${current_user[0].name}`
 }
 
+// search box JS
+const searchBar = forms['search-messages'].querySelector('input');
+searchBar.addEventListener('keyup', (e) => {
+  const term = e.target.value.toLowerCase();
+  const messageList = list.getElementsByClassName('userMsg');
+  Array.from(messageList).forEach((msgList) => {
+    const title = msgList.textContent;
+    if (title.toLowerCase().indexOf(e.target.value) !== -1) {
+      msgList.parentNode.style.display = 'block';
+    } else {
+      msgList.parentNode.style.display = 'none';
+    }
+  });
+});
 sendBtn.addEventListener('mousedown', () => {
   if (input.value) {
-    sendBtn.innerHTML = 'Sent &nbsp;<i class="fas fa-chevron-circle-right"></i>';
-    sendBtn.style.backgroundColor = '#38b000';
+    sendBtn.innerHTML = 'Sent &nbsp;<i class="fas fa-chevron-circle-right"></i>'
+    sendBtn.style.backgroundColor = '#38b000'
   }
 });
 
 sendBtn.addEventListener('mouseup', () => {
   setTimeout(() => {
-    sendBtn.innerHTML = 'Send <i class="fas fa-chevron-circle-right"></i>';
-    sendBtn.style.backgroundColor = '#ed1c24';
+    sendBtn.innerHTML = 'Send <i class="fas fa-chevron-circle-right"></i>'
+    sendBtn.style.backgroundColor = '#ed1c24'
   }, 400);
 });
 
