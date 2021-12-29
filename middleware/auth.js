04c0550fba5b4e53bcec05740c0ef9addb80c57d
snahashis <snahashiskanrar@gmail.com>
2021-12-29 21:59:58 +0530

    Fixed Few Indentations

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,27 +1,25 @@
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-const requireauth=(req,res,next)=>{
-    const token=req.cookies.login;
-    if(token)
-    {
-        jwt.verify(token,"ankitgarg",(err, decodedToken)=>{
-            if(err)
-            {
+const requireauth = (req, res, next) => {
+    const token = req.cookies.login;
+    if (token) {
+        jwt.verify(token, "ankitgarg", (err, decodedToken) => {
+            if (err) {
                 console.log(err.message);
                 res.redirect("/login");
             }
-                else{
-                    console.log(decodedToken);
-                    next();
-                }
+            else {
+                console.log(decodedToken);
+                next();
             }
+        }
         )
     }
-    else{
+    else {
         res.redirect("/login");
     }
 };
 
 
 
-module.exports={requireauth};
+module.exports = { requireauth };
