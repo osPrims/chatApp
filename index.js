bc0a85f503af38344a11a5738c86178b73f99186
Jigyasa Gupta <75614134+Jigyasa-G@users.noreply.github.com>
2022-01-03 09:21:45 +0530

    Formatted

--- index.js
+++ index.js
@@ -220,9 +220,10 @@ app.post("/otp", async (req, res) => {
           from:process.env.EMAIL,
           to: req.body.email,
           subject: "ChatApp OTP to login - Forgot password",
-          text:'Hello user, **Your OTP is : ${otp}** Enter this code within 1 hours to login to your account
-          'if you have forgotten your password or go to the login page to resend it. If you do not recognize'
-          'or expect this mail, please do not share the above OTP with anyone.Regards Team ChatApp`
+          text:'Hello user,
+          'Your OTP is : ${otp}
+          'Enter this code within 1 hour to login to your account if you have forgotten your password or go to the login page to resend it. If you do not recognize or expect this mail, please do not share the above OTP with anyone.
+          'Regards Team ChatApp`
             
         }
 
