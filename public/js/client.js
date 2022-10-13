2fbf75a551d02be52e205b78581998cb02b45f64
ozair-dev <truemail785@gmail.com>
2022-10-13 18:23:25 +0500

    #189-fix redundant messages

--- public/js/client.js
+++ public/js/client.js
@@ -3,15 +3,15 @@ let form = document.getElementById("form");
 let input = document.getElementById("input");
 let feedback = document.getElementById("feedback");
 let username = document.getElementById("username");
-let messages = document.getElementById('messages')
-let online = document.getElementById('online');
-let sendBtn = document.querySelector('.btn--send');
-let list = document.querySelector('#messages');
+let messages = document.getElementById("messages");
+let online = document.getElementById("online");
+let sendBtn = document.querySelector(".btn--send");
+let list = document.querySelector("#messages");
 let forms = document.forms;
-let users = []
-let selfId
-let md
-let myId
+let users = [];
+let selfId, selfMail;
+let md;
+let myId;
 
 let input_file = document.getElementById("input_file");
 let label = document.getElementsByClassName("file");
@@ -22,19 +22,29 @@ md = window.markdownit({
   linkify: true,
   typographer: true,
   breaks: true,
-});;
+});
 
 fetch("/me")
   .then((user) => user.json())
   .then((data) => {
     console.log(data);
-    document.getElementById('username_holder').innerText = data.username;
-    document.getElementById('email_holder').innerText = `<${data.email}>`;
-    myId = data
-  })
+    document.getElementById("username_holder").innerText = data.username;
+    document.getElementById("email_holder").innerText = `<${data.email}>`;
+    myId = data;
+  });
 
 // Color for the messages
-let colors = ['#0080FF', '#8000FF', '#FF00FF', '#FF0080', '#FF0000', '#FF8000', '#80FF00', '#00FF00', '#00FF80']
+let colors = [
+  "#0080FF",
+  "#8000FF",
+  "#FF00FF",
+  "#FF0080",
+  "#FF0000",
+  "#FF8000",
+  "#80FF00",
+  "#00FF00",
+  "#00FF80",
+];
 
 // let coll = document.getElementsByClassName("collapsible");
 
@@ -53,11 +63,12 @@ let addusertolist = (user) => {
   let item = document.createElement("li");
   // item.style.color = (selfId) ? user.color : 'blue';
   item.className = "clearfix";
-  item.innerHTML = '<div class="about"><div class="name">' + user.name + '</div></div>';
-  item.id = user.id
-  item.onclick = handleOnlineClick.bind(null, user.id)
+  item.innerHTML =
+    '<div class="about"><div class="name">' + user.name + "</div></div>";
+  item.id = user.id;
+  item.onclick = handleOnlineClick.bind(null, user.id);
   online.appendChild(item);
-}
+};
 
 // Fetch users online as soon as you connect
 fetch("/users")
@@ -66,12 +77,12 @@ fetch("/users")
     console.log("asdasd");
     console.log(data);
     data.forEach((user) => {
-      user.color = colors[0]
-      colors = colors.splice(1)
-      addusertolist(user)
-    })
-    users = users.concat(data)
-  })
+      user.color = colors[0];
+      colors = colors.splice(1);
+      addusertolist(user);
+    });
+    users = users.concat(data);
+  });
 
 // Fetch messages as soon as you connect
 // fetch("/messages")
@@ -108,45 +119,52 @@ form.addEventListener("submit", (e) => {
 });
 
 // Received from server when someone gets connected
-socket.on("connected", ({ id, name }) => {
-
-  users.push({ name: name, id: id, color: colors[0] })
-  addusertolist({ name: name, id: id, color: colors[0] })
-  colors = colors.splice(1)
+socket.on("connected", ({ id, name, email }) => {
+  // update user's (socket) id when reconnected
+  if (selfMail === email) {
+    const userIdx = users.findIndex((u) => u.email === email);
+    users[userIdx].id = id;
+    selfId = id;
+    return;
+  }
 
   if (selfId) {
     let item = document.createElement("li");
-    item.className = 'connection p-2 rounded'
+    item.className = "connection p-2 rounded";
     item.style.color = "black";
     item.textContent = ` ${name} has connected`;
     item.style.backgroundColor = "LightGray";
     messages.appendChild(item);
-  }
-  else {
-    selfId = id
-    feedback.innerHTML = "Welcome to Chat App - Instant Messaging App"
-    
+  } else {
+    // Set user id and email when connected for first time
+    selfId = id;
+    selfMail = email;
+    feedback.innerHTML = "Welcome to Chat App - Instant Messaging App";
   }
 
-  scrollSmoothToBottom('main')
+  users.push({ name, id, email, color: colors[0] });
+  addusertolist({ name, id, email, color: colors[0] });
+  colors = colors.splice(1);
+
+  scrollSmoothToBottom("main");
 });
 
 // Received from server when someone gets disconnected
 socket.on("disconnected", (id) => {
-  let current_user = users.filter((user) => user.id === id)
-  colors.push(current_user[0].color)
+  let current_user = users.filter((user) => user.id === id);
+  colors.push(current_user[0].color);
   users = users.filter((user) => user.id != id);
 
   let item = document.createElement("li");
   item.style.color = "black";
-  item.className = 'connection p-2'
+  item.className = "connection p-2";
   item.style.backgroundColor = "LightGray";
-  item.textContent = current_user[0].name + ' has disconnected'
+  item.textContent = current_user[0].name + " has disconnected";
   messages.appendChild(item);
 
-  scrollSmoothToBottom('main')
+  scrollSmoothToBottom("main");
   removeuserfromlist(id);
-  feedback.innerHTML = ''
+  feedback.innerHTML = "";
 });
 
 // Recieved from a server when a chat message is received
@@ -155,31 +173,36 @@ socket.on("chat message", (user, msg, time, toUser) => {
   let item = document.createElement("li");
   item.className = user.id + " clearfix position-relative";
 
-  let current_user = users.filter((_user_) => _user_.id === user.id)
+  let current_user = users.filter((_user_) => _user_.id === user.id);
   if (selfId === user.id) {
-    item.innerHTML = `<div class="message other-message bg-custom text-white ls-msg float-right p-3 wordwrap"> <span class="pb-2 fw-bold">${user.name}</span><br>${md.render(msg)}</div><span class="text-muted position-absolute bottom--10 end-0 fs-6">${time} </span>`;
+    item.innerHTML = `<div class="message other-message bg-custom text-white ls-msg float-right p-3 wordwrap"> <span class="pb-2 fw-bold">${
+      user.name
+    }</span><br>${md.render(
+      msg
+    )}</div><span class="text-muted position-absolute bottom--10 end-0 fs-6">${time} </span>`;
     // item.classList.add('self')
-  }
-  else {
+  } else {
     // item.style.color = current_user[0].color
-    item.innerHTML = `<div class="message my-message ls-msg p-3 wordwrap"><span class="pb-2 fw-bold">${user.name}</span><br>${md.render(msg)}</div><span class="text-muted position-absolute bottom--10 start-0 fs-6">${time} </span>`;
+    item.innerHTML = `<div class="message my-message ls-msg p-3 wordwrap"><span class="pb-2 fw-bold">${
+      user.name
+    }</span><br>${md.render(
+      msg
+    )}</div><span class="text-muted position-absolute bottom--10 start-0 fs-6">${time} </span>`;
   }
-
 
   // if (toUser !== "null") item.innerHTML = `<b>${user.name}&nbsp;</b><b>toUser: ${toUser.name}&nbsp;</b> <span class="time">${time} </span>` + `<div class="userMsg">${md.render(msg)}</div>`;
   // else item.innerHTML = `<b>${user.name}&nbsp;</b> <span class="time">${time} </span>` + `<div class="userMsg">${md.render(msg)}</div>`;
   // item.innerHTML = `<b>${ user.name }: </b>` + `<div class="userMsg">${msg}</div>`;
   // item.classList.add('messages')
-  messages.appendChild(item)
+  messages.appendChild(item);
 
-  scrollSmoothToBottom('main')
-  if (user.id !== selfId) 
-  feedback.innerHTML = "";
+  scrollSmoothToBottom("main");
+  if (user.id !== selfId) feedback.innerHTML = "";
 
   // check if someone has set their name
   users.forEach((saved_user) => {
     if (saved_user.id === user.id) {
-      saved_user.name = user.name
+      saved_user.name = user.name;
       let item = document.getElementById(user.id);
       item.innerHTML = `<div class="about">${user.name}</div>`;
     }
@@ -196,26 +219,35 @@ socket.on("output", ({ result, useremail }) => {
 
       if (result[x].email == useremail) {
         // item.classList.add("useridentified");
-        item.innerHTML = `<div class="message other-message bg-custom text-white ls-msg float-right p-3 wordwrap"><span class="pb-2 fw-bold">${result[x].name}</span><br>${md.render(result[x].message)}</div><span class="text-muted position-absolute bottom--10 end-0 fs-6">${result[x].time} </span>`;
-      }
-      else {
+        item.innerHTML = `<div class="message other-message bg-custom text-white ls-msg float-right p-3 wordwrap"><span class="pb-2 fw-bold">${
+          result[x].name
+        }</span><br>${md.render(
+          result[x].message
+        )}</div><span class="text-muted position-absolute bottom--10 end-0 fs-6">${
+          result[x].time
+        } </span>`;
+      } else {
         // item.classList.add('messages');
-        item.innerHTML = `<div class="message my-message ls-msg p-3 wordwrap"><span class="pb-2 fw-bold">${result[x].name}</span><br>${md.render(result[x].message)}</div><span class="text-muted position-absolute bottom--10 start-0 fs-6">${result[x].time} </span>`;
+        item.innerHTML = `<div class="message my-message ls-msg p-3 wordwrap"><span class="pb-2 fw-bold">${
+          result[x].name
+        }</span><br>${md.render(
+          result[x].message
+        )}</div><span class="text-muted position-absolute bottom--10 start-0 fs-6">${
+          result[x].time
+        } </span>`;
       }
       messages.appendChild(item);
     }
   }
-  scroll('main')
+  scroll("main");
 });
 function scroll(id) {
-  var div = document.getElementById(id)
-  var scrollHeight = div.scrollHeight
+  var div = document.getElementById(id);
+  var scrollHeight = div.scrollHeight;
   div.scroll({
     top: scrollHeight + 10,
-
-  })
+  });
 }
-
 
 // Sent to server when you type
 input.addEventListener("keypress", () => {
@@ -233,102 +265,100 @@ socket.on("typing", (user) => {
 });
 // Recieved from a server when an image file is received
 socket.on("base64_file", (data, time) => {
-
-  var listitem = document.createElement('li');
+  var listitem = document.createElement("li");
   listitem.className = "clearfix position-relative";
   var curr_user_img = users.filter((_user_) => _user_.id === data.id);
   if (selfId === data.id) {
-    listitem.innerHTML = `<div class = "message other-message bg-custom text-white ls-msg float-right p-3 wordwrap"><span class = "pb-2 fw-bold"><b>${data.username}&nbsp;</b><br><img  src="${data.file}" height="200" width="200"/></div></span><span class = "text-muted position-absolute bottom--10 end-0 fs-6">${time} </span>`
+    listitem.innerHTML = `<div class = "message other-message bg-custom text-white ls-msg float-right p-3 wordwrap"><span class = "pb-2 fw-bold"><b>${data.username}&nbsp;</b><br><img  src="${data.file}" height="200" width="200"/></div></span><span class = "text-muted position-absolute bottom--10 end-0 fs-6">${time} </span>`;
     input_file.value = "";
-  }
-  else {
-    listitem.innerHTML = `<div class = "message my-message ls-msg p-3 wordwrap"><span class = "pb-2 fw-bold"><b>${data.username}&nbsp;</b><br><img  src="${data.file}" height="200" width="200"/></div></span><span class="text-muted position-absolute bottom--10 start-0 fs-6">${time} </span>`
+  } else {
+    listitem.innerHTML = `<div class = "message my-message ls-msg p-3 wordwrap"><span class = "pb-2 fw-bold"><b>${data.username}&nbsp;</b><br><img  src="${data.file}" height="200" width="200"/></div></span><span class="text-muted position-absolute bottom--10 start-0 fs-6">${time} </span>`;
   }
 
   //listitem.classList.add('messages')
   messages.appendChild(listitem);
-  if (data.id !== selfId) 
-  feedback.innerHTML = "";
+  if (data.id !== selfId) feedback.innerHTML = "";
 
-  scrollSmoothToBottom('main')
-
+  scrollSmoothToBottom("main");
 });
 
 // Remove use from collapsible
 let removeuserfromlist = (userid) => {
   let item = document.getElementById(userid);
-  if (item != null)
-    item.remove();
-}
+  if (item != null) item.remove();
+};
 
 // Auto scroll to bottom when messages come
 function scrollSmoothToBottom(id) {
-  var div = document.getElementById(id)
-  var scrollHeight = div.scrollHeight
+  var div = document.getElementById(id);
+  var scrollHeight = div.scrollHeight;
   div.scroll({
     top: scrollHeight + 10,
-    behavior: "smooth"
-  })
+    behavior: "smooth",
+  });
 }
 
 function handleOnlineClick(id) {
-  let current_user = users.filter((user) => user.id === id)
-  input.value = `@${current_user[0].name}`
+  let current_user = users.filter((user) => user.id === id);
+  input.value = `@${current_user[0].name}`;
 }
 
 // search box JS
-const searchBar = forms['search-messages'].querySelector('input');
+const searchBar = forms["search-messages"].querySelector("input");
 
-document.getElementById("search-messages").addEventListener("submit", function (event) {
-  event.preventDefault()
-});
+document
+  .getElementById("search-messages")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
 
-searchBar.addEventListener('keyup', (e) => {
-  e.preventDefault()
+searchBar.addEventListener("keyup", (e) => {
+  e.preventDefault();
   const term = e.target.value.toLowerCase();
-  const messageList = list.getElementsByClassName('ls-msg');
+  const messageList = list.getElementsByClassName("ls-msg");
   Array.from(messageList).forEach((msgList) => {
     const title = msgList.textContent;
     if (title.toLowerCase().indexOf(e.target.value) !== -1) {
-      msgList.parentNode.style.display = 'block';
+      msgList.parentNode.style.display = "block";
     } else {
-      msgList.parentNode.style.display = 'none';
+      msgList.parentNode.style.display = "none";
     }
   });
 });
-sendBtn.addEventListener('mousedown', () => {
+sendBtn.addEventListener("mousedown", () => {
   if (input.value || input_file.value) {
-    sendBtn.innerHTML = 'Sent &nbsp;<i class="fas fa-chevron-circle-right"></i>'
-    sendBtn.style.backgroundColor = '#38b000'
+    sendBtn.innerHTML =
+      'Sent &nbsp;<i class="fas fa-chevron-circle-right"></i>';
+    sendBtn.style.backgroundColor = "#38b000";
   }
 });
 
-sendBtn.addEventListener('mouseup', () => {
+sendBtn.addEventListener("mouseup", () => {
   setTimeout(() => {
-    sendBtn.innerHTML = 'Send <i class="fas fa-chevron-circle-right"></i>'
-    sendBtn.style.backgroundColor = '#ed1c24'
+    sendBtn.innerHTML = 'Send <i class="fas fa-chevron-circle-right"></i>';
+    sendBtn.style.backgroundColor = "#ed1c24";
   }, 400);
 });
 
 input.addEventListener("keyup", function (event) {
-  if (event.key === 'Enter') {
+  if (event.key === "Enter") {
     sendBtn.click();
-    sendBtn.style.backgroundColor = '#38b000'
+    sendBtn.style.backgroundColor = "#38b000";
     setTimeout(() => {
-      sendBtn.style.backgroundColor = '#ed1c24'
+      sendBtn.style.backgroundColor = "#ed1c24";
     }, 400);
   }
 });
 
 function readThensend() {
-  const data = document.querySelector('input[type=file]').files[0];
+  const data = document.querySelector("input[type=file]").files[0];
   const reader = new FileReader();
   reader.onload = function (evt) {
     var msg = {};
     msg.username = socket.name;
     msg.file = evt.target.result;
     msg.fileName = data.fileName;
-    socket.emit("base64_file", msg);
+    socket.emit("base64_file", msg, getTime());
   };
   reader.readAsDataURL(data);
 }
@@ -341,8 +371,12 @@ function sendposition(position) {
   var currentLatitude = position.coords.latitude;
   var currentLongitude = position.coords.longitude;
 
-  var infoWindowHTML = "Latitude: " + currentLatitude + "<br>Longitude: " + currentLongitude;
-  var infoWindow = new google.maps.InfoWindow({ map: map, content: infoWindowHTML });
+  var infoWindowHTML =
+    "Latitude: " + currentLatitude + "<br>Longitude: " + currentLongitude;
+  var infoWindow = new google.maps.InfoWindow({
+    map: map,
+    content: infoWindowHTML,
+  });
   var currentLocation = { lat: currentLatitude, lng: currentLongitude };
   infoWindow.setPosition(currentLocation);
   socket.emit("location_Send", infoWindow);
@@ -357,10 +391,10 @@ function getTime() {
   let date = new Date();
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  let meridian = hours >= 12 ? 'PM' : 'AM';
+  let meridian = hours >= 12 ? "PM" : "AM";
   hours = hours % 12;
   hours = hours ? hours : 12;
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  let strTime = hours + ':' + minutes + ' ' + meridian;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  let strTime = hours + ":" + minutes + " " + meridian;
   return strTime;
 }
