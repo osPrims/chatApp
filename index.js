dfc5f7509e1e9b6b409e716975bb2168aa8ca26e
ozair-dev <truemail785@gmail.com>
2022-10-13 18:23:25 +0500

    #189-fix redundant messages

--- index.js
+++ index.js
@@ -104,7 +104,6 @@ app.get("/ui", requireauth, (req, res) => {
   res.sendFile(__dirname + "/tmp/old.index.html");
 });
 
-
 //handling signup
 app.get("/signup", (req, res) => {
   res.sendFile(__dirname + "/views/signup.html");
@@ -113,7 +112,7 @@ app.get("/signup", (req, res) => {
 //handling sign post request
 app.post("/signup", async (req, res) => {
   try {
-    console.log(req.body)
+    console.log(req.body);
     if (req.body.password === req.body.conpassword) {
       const user = new User({
         username: req.body.username,
@@ -190,49 +189,42 @@ app.post("/otp", async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (!user) {
       throw "Invalid Email";
-
-    }
-    else {
-
+    } else {
       var email;
 
       let transporter = nodemailer.createTransport({
-
         host: "smtp.gmail.com",
         port: 465,
         secure: true,
-        service: 'Gmail',
+        service: "Gmail",
 
         auth: {
           user: process.env.EMAIL,
-          pass: process.env.PASSWORD
+          pass: process.env.PASSWORD,
         },
       });
       let otp = Math.random();
       otp = otp * 1000000;
       otp = parseInt(otp);
       console.log(otp);
-      
-        // send mail with defined transport object
-        var mailOptions = {
-          from:process.env.EMAIL,
-          to: req.body.email,
-          subject: "Reset Password OTP | ChatApp",
-          text: `Hello user,\nYour OTP is : ${otp}\nEnter this code within 1 hour to login to your account if you have forgotten your password or go to the login page to resend it. If you do not recognize or expect this mail, please do not share the above OTP with anyone.\n\nchatApp`
 
+      // send mail with defined transport object
+      var mailOptions = {
+        from: process.env.EMAIL,
+        to: req.body.email,
+        subject: "Reset Password OTP | ChatApp",
+        text: `Hello user,\nYour OTP is : ${otp}\nEnter this code within 1 hour to login to your account if you have forgotten your password or go to the login page to resend it. If you do not recognize or expect this mail, please do not share the above OTP with anyone.\n\nchatApp`,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        } else {
+          console.log("done");
         }
-
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            return console.log(error);
-          }
-          else{
-            console.log("done");
-          }
-
       });
 
-      res.json({ otp })
+      res.json({ otp });
     }
   } catch (err) {
     let error = { email: "" };
@@ -247,16 +239,13 @@ app.get("/forgotpassword", (req, res) => {
   res.sendFile(__dirname + "/views/fpassword.html");
 });
 
-
 app.post("/forgotpassword", async (req, res) => {
   try {
-    console.log(req.body.otp, parseInt(req.body.userotp))
+    console.log(req.body.otp, parseInt(req.body.userotp));
     if (req.body.otp != parseInt(req.body.userotp)) {
-
       throw "Invalid Otp";
-    }
-    else {
-      console.log("noerror")
+    } else {
+      console.log("noerror");
       if (req.body.password === req.body.conpassword) {
         if (req.body.password.length >= 6) {
           const salt = await bcrypt.genSalt();
@@ -300,7 +289,7 @@ app.get("/messages", async (req, res) => {
 });
 
 app.get("/logout", (req, res) => {
-  res.cookie("login", "", { maxAge: 1 })
+  res.cookie("login", "", { maxAge: 1 });
   res.redirect("/login");
 });
 
@@ -308,89 +297,94 @@ app.get("/logout", (req, res) => {
 
 // When a connection is received
 io.on("connection", (socket) => {
-  console.log("A user has connected");
-  io.emit("connected", { id: socket.id, name: userentered });
-  getmessages(socket);
-
-  socket.name = "";
-  let filtered_users = users.filter((user) => user.id == socket.id);
-  if (filtered_users != []) {
-    users.push({
-      name: userentered,
+  if (user1) {
+    console.log("A user has connected");
+    io.emit("connected", {
       id: socket.id,
+      name: userentered,
       email: useremail,
     });
-  }
+    getmessages(socket);
 
-  // Receiving a chat message from client
-  socket.on("mychat message", (msg, time) => {
-    console.log("Received a chat message");
-    let current_user = users.filter((user) => user.id === socket.id);
-    const mail = current_user[0].email;
-    const name = current_user[0].name;
-    socket.name = name;
-
-    let userList = [];
-    if (msg.substr(0, 3) == "/w ") {
-      msg = msg.substr(3);
-      const idx = msg.indexOf(" ");
-
-      if (idx != -1) {
-        const toUsername = msg.substr(0, idx);
-        msg = msg.substr(idx + 1);
-        userList = users.filter((_user_) => _user_.name === toUsername);
-      }
+    socket.name = "";
+    let filtered_users = users.filter((user) => user.id == socket.id);
+    if (filtered_users != []) {
+      users.push({
+        name: userentered,
+        id: socket.id,
+        email: useremail,
+      });
     }
 
-    if (userList.length)
-      userList.forEach((user) =>
-        io
-          .to(socket.id)
-          .to(user.id)
-          .emit(
-            "chat message",
-            { name: socket.name, id: socket.id },
-            msg,
-            time,
-            user
-          )
-      );
-    else
-      io.emit(
-        "chat message",
-        { name: socket.name, id: socket.id },
-        msg,
-        time,
-        "null"
-      );
+    // Receiving a chat message from client
+    socket.on("mychat message", (msg, time) => {
+      console.log("Received a chat message");
+      let current_user = users.filter((user) => user.id === socket.id);
+      const mail = current_user[0].email;
+      const name = current_user[0].name;
+      socket.name = name;
 
-    storemessage(name, msg, mail, time);
-  });
+      let userList = [];
+      if (msg.substr(0, 3) == "/w ") {
+        msg = msg.substr(3);
+        const idx = msg.indexOf(" ");
 
-  // Received when some client is typing
-  socket.on("typing", (user) => {
-    socket.broadcast.emit("typing", user);
-  });
-  // Receiving an image file from client
-  socket.on("base64_file", function (msg) {
-    let current_user = users.filter((user) => user.id === socket.id);
-    const name = current_user[0].name;
-    socket.name = name;
-    console.log(`received base64 file from ${socket.name}`);
-    var data = {};
-    data.fileName = msg.fileName;
-    data.file = msg.file;
-    data.id = socket.id
-    data.username = (socket.name == '' ? "Anonymous" : socket.name);
-    io.sockets.emit("base64_file", data, time);
+        if (idx != -1) {
+          const toUsername = msg.substr(0, idx);
+          msg = msg.substr(idx + 1);
+          userList = users.filter((_user_) => _user_.name === toUsername);
+        }
+      }
 
-  });
-  // Sent to all clients when a socket is disconnected
-  socket.on("disconnect", () => {
-    console.log("A user has disconnected");
-    users = users.filter((user) => user.id !== socket.id);
-    io.emit("disconnected", socket.id);
-  });
+      if (userList.length)
+        userList.forEach((user) =>
+          io
+            .to(socket.id)
+            .to(user.id)
+            .emit(
+              "chat message",
+              { name: socket.name, id: socket.id },
+              msg,
+              time,
+              user
+            )
+        );
+      else
+        io.emit(
+          "chat message",
+          { name: socket.name, id: socket.id },
+          msg,
+          time,
+          "null"
+        );
+
+      storemessage(name, msg, mail, time);
+    });
+
+    // Received when some client is typing
+    socket.on("typing", (user) => {
+      socket.broadcast.emit("typing", user);
+    });
+    // Receiving an image file from client
+    socket.on("base64_file", function (msg, time) {
+      let current_user = users.filter((user) => user.id === socket.id);
+      const name = current_user[0].name;
+      socket.name = name;
+      console.log(`received base64 file from ${socket.name}`);
+      var data = {};
+      data.fileName = msg.fileName;
+      data.file = msg.file;
+      data.id = socket.id;
+      data.username = socket.name == "" ? "Anonymous" : socket.name;
+      io.sockets.emit("base64_file", data, time);
+    });
+    // Sent to all clients when a socket is disconnected
+    socket.on("disconnect", () => {
+      console.log("A user has disconnected");
+      users = users.filter((user) => user.id !== socket.id);
+      io.emit("disconnected", socket.id);
+    });
+  }
 });
 
 server.listen(port, () => {
