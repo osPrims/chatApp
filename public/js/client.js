d384f926e481acb8a46a8039762d3bafe8f41a26
Karolina <karolina@MacBook-Air-Karolina.local>
2023-01-03 13:36:07 +0100

    Fix variables and add comments to functions

--- public/js/client.js
+++ public/js/client.js
@@ -7,31 +7,14 @@ let messages = document.getElementById("messages");
 let online = document.getElementById("online");
 let sendBtn = document.querySelector(".btn--send");
 let list = document.querySelector("#messages");
-let forms = document.forms;
+let inputFile = document.getElementById("input_file");
+let searchBar = document.getElementById("search-messages");
+let searchBarInput = document.getElementById("search-messages").querySelector("input");
 let users = [];
-let selfId, selfMail;
+let selfId;
+let selfMail;
 let md;
 let myId;
-
-let input_file = document.getElementById("input_file");
-let label = document.getElementsByClassName("file");
-
-md = window.markdownit({
-  html: false,
-  xhtmlOut: false,
-  linkify: true,
-  typographer: true,
-  breaks: true,
-});
-
-fetch("/me")
-  .then((user) => user.json())
-  .then((data) => {
-    console.log(data);
-    document.getElementById("username_holder").innerText = data.username;
-    document.getElementById("email_holder").innerText = `<${data.email}>`;
-    myId = data;
-  });
 
 // Color for the messages
 let colors = [
@@ -46,12 +29,30 @@ let colors = [
   "#00FF80",
 ];
 
+// Markdown for the messages
+md = window.markdownit({
+  html: false,
+  xhtmlOut: false,
+  linkify: true,
+  typographer: true,
+  breaks: true,
+});
+
+// Fetch user
+fetch("/me")
+  .then((user) => user.json())
+  .then((data) => {
+    document.getElementById("username_holder").innerText = data.username;
+    document.getElementById("email_holder").innerText = `<${data.email}>`;
+    myId = data;
+  });
+
+
 // Add user to collapsible
-let addusertolist = (user) => {
+let addUserToList = (user) => {
   let item = document.createElement("li");
   item.className = "clearfix";
-  item.innerHTML =
-    '<div class="about"><div class="name">' + user.name + "</div></div>";
+  item.innerHTML = '<div class="about"><div class="name">' + user.name + "</div></div>";
   item.id = user.id;
   item.onclick = handleOnlineClick.bind(null, user.id);
   online.appendChild(item);
@@ -61,35 +62,32 @@ let addusertolist = (user) => {
 fetch("/users")
   .then((user) => user.json())
   .then((data) => {
-    console.log("asdasd");
-    console.log(data);
     data.forEach((user) => {
       user.color = colors[0];
       colors = colors.splice(1);
-      addusertolist(user);
+      addUserToList(user);
     });
     users = users.concat(data);
   });
 
-// Sent a chat message to server when submit a form
+// Send a chat message to server when submitting the form
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value) {
     socket.emit("mychat message", input.value, getTime());
-    // console.log("send", input.value);
     input.value = "";
   }
 });
 
 // Received from server when someone gets connected
 socket.on("connected", ({ id, name, email }) => {
-  // update user's (socket) id when reconnected
+  // Update user's (socket) id when reconnected
   if (selfMail === email) {
     const userIdx = users.findIndex((u) => u.email === email);
     users[userIdx].id = id;
     selfId = id;
     return;
-  }
+  } 
 
   if (selfId) {
     let item = document.createElement("li");
@@ -106,7 +104,7 @@ socket.on("connected", ({ id, name, email }) => {
   }
 
   users.push({ name, id, email, color: colors[0] });
-  addusertolist({ name, id, email, color: colors[0] });
+  addUserToList({ name, id, email, color: colors[0] });
   colors = colors.splice(1);
 
   scrollSmoothToBottom("main");
@@ -114,29 +112,28 @@ socket.on("connected", ({ id, name, email }) => {
 
 // Received from server when someone gets disconnected
 socket.on("disconnected", (id) => {
-  let current_user = users.filter((user) => user.id === id);
-  colors.push(current_user[0].color);
+  let currentUser = users.filter((user) => user.id === id);
+  colors.push(currentUser[0].color);
   users = users.filter((user) => user.id != id);
 
   let item = document.createElement("li");
   item.style.color = "black";
   item.className = "connection p-2";
   item.style.backgroundColor = "LightGray";
-  item.textContent = current_user[0].name + " has disconnected";
+  item.textContent = currentUser[0].name + " has disconnected";
   messages.appendChild(item);
 
   scrollSmoothToBottom("main");
-  removeuserfromlist(id);
+  removeUserFromList(id);
   feedback.innerHTML = "";
 });
 
 // Recieved from a server when a chat message is received
 socket.on("chat message", (user, msg, time, toUser) => {
-  console.log(user, msg, time, toUser);
   let item = document.createElement("li");
   item.className = user.id + " clearfix position-relative";
 
-  let current_user = users.filter((_user_) => _user_.id === user.id);
+  let currentUser = users.filter((_user_) => _user_.id === user.id);
   if (selfId === user.id) {
     item.innerHTML = `<div class="message other-message bg-custom text-white ls-msg float-right p-3 wordwrap"> <span class="pb-2 fw-bold">${
       user.name
@@ -156,24 +153,24 @@ socket.on("chat message", (user, msg, time, toUser) => {
   scrollSmoothToBottom("main");
   if (user.id !== selfId) feedback.innerHTML = "";
 
-  // check if someone has set their name
-  users.forEach((saved_user) => {
-    if (saved_user.id === user.id) {
-      saved_user.name = user.name;
+  // Check if someone has set their name
+  users.forEach((savedUser) => {
+    if (savedUser.id === user.id) {
+      savedUser.name = user.name;
       let item = document.getElementById(user.id);
       item.innerHTML = `<div class="about">${user.name}</div>`;
     }
   });
 });
+
+// Fetch messages after logging in
 socket.on("output", ({ result, useremail }) => {
-  console.log(result);
   if (result.length) {
-    for (var x = 0; x < result.length; x++) {
+    for (let x = 0; x < result.length; x++) {
       let item = document.createElement("li");
       item.className = "clearfix position-relative";
 
       if (result[x].email == useremail) {
-        // item.classList.add("useridentified");
         item.innerHTML = `<div class="message other-message bg-custom text-white ls-msg float-right p-3 wordwrap"><span class="pb-2 fw-bold">${
           result[x].name
         }</span><br>${md.render(
@@ -182,7 +179,6 @@ socket.on("output", ({ result, useremail }) => {
           result[x].time
         } </span>`;
       } else {
-        // item.classList.add('messages');
         item.innerHTML = `<div class="message my-message ls-msg p-3 wordwrap"><span class="pb-2 fw-bold">${
           result[x].name
         }</span><br>${md.render(
@@ -196,9 +192,11 @@ socket.on("output", ({ result, useremail }) => {
   }
   scroll("main");
 });
+
+// Scroll to bottom
 function scroll(id) {
-  var div = document.getElementById(id);
-  var scrollHeight = div.scrollHeight;
+  let div = document.getElementById(id);
+  let scrollHeight = div.scrollHeight;
   div.scroll({
     top: scrollHeight + 10,
   });
@@ -210,66 +208,64 @@ input.addEventListener("keypress", () => {
 });
 
 //Received from server when someone else is typing
-let fbTimer;
 socket.on("typing", (user) => {
+  let fbTimer;
   clearTimeout(fbTimer);
   feedback.innerHTML = user + " is typing...";
   fbTimer = setTimeout(() => {
     feedback.innerHTML = "";
   }, 2000);
 });
-// Recieved from a server when an image file is received
+
+// Recieved from server when an image file is received
 socket.on("base64_file", (data, time) => {
-  var listitem = document.createElement("li");
-  listitem.className = "clearfix position-relative";
-  var curr_user_img = users.filter((_user_) => _user_.id === data.id);
+  let listItem = document.createElement("li");
+  listItem.className = "clearfix position-relative";
+  let currUserImg = users.filter((_user_) => _user_.id === data.id);
   if (selfId === data.id) {
-    listitem.innerHTML = `<div class = "message other-message bg-custom text-white ls-msg float-right p-3 wordwrap"><span class = "pb-2 fw-bold"><b>${data.username}&nbsp;</b><br><img  src="${data.file}" height="200" width="200"/></div></span><span class = "text-muted position-absolute bottom--10 end-0 fs-6">${time} </span>`;
-    input_file.value = "";
+    listItem.innerHTML = `<div class = "message other-message bg-custom text-white ls-msg float-right p-3 wordwrap"><span class = "pb-2 fw-bold"><b>${data.username}&nbsp;</b><br><img  src="${data.file}" height="200" width="200"/></div></span><span class = "text-muted position-absolute bottom--10 end-0 fs-6">${time} </span>`;
+    inputFile.value = "";
   } else {
-    listitem.innerHTML = `<div class = "message my-message ls-msg p-3 wordwrap"><span class = "pb-2 fw-bold"><b>${data.username}&nbsp;</b><br><img  src="${data.file}" height="200" width="200"/></div></span><span class="text-muted position-absolute bottom--10 start-0 fs-6">${time} </span>`;
+    listItem.innerHTML = `<div class = "message my-message ls-msg p-3 wordwrap"><span class = "pb-2 fw-bold"><b>${data.username}&nbsp;</b><br><img  src="${data.file}" height="200" width="200"/></div></span><span class="text-muted position-absolute bottom--10 start-0 fs-6">${time} </span>`;
   }
 
-  messages.appendChild(listitem);
+  messages.appendChild(listItem);
   if (data.id !== selfId) feedback.innerHTML = "";
 
   scrollSmoothToBottom("main");
 });
 
-// Remove use from collapsible
-let removeuserfromlist = (userid) => {
-  let item = document.getElementById(userid);
+// Remove user from collapsible
+let removeUserFromList = (userId) => {
+  let item = document.getElementById(userId);
   if (item != null) item.remove();
 };
 
-// Auto scroll to bottom when messages come
+// Smooth scroll to bottom
 function scrollSmoothToBottom(id) {
-  var div = document.getElementById(id);
-  var scrollHeight = div.scrollHeight;
+  let div = document.getElementById(id);
+  let scrollHeight = div.scrollHeight;
   div.scroll({
     top: scrollHeight + 10,
     behavior: "smooth",
   });
 }
 
+// Mention user 
 function handleOnlineClick(id) {
-  let current_user = users.filter((user) => user.id === id);
-  input.value = `@${current_user[0].name}`;
+  let currentUser = users.filter((user) => user.id === id);
+  input.value = `@${currentUser[0].name}`;
 }
 
-// search box JS
-const searchBar = forms["search-messages"].querySelector("input");
-
-document
-  .getElementById("search-messages")
-  .addEventListener("submit", function (event) {
+// Search for a phrase in messages
+searchBar.addEventListener("submit", function (event) {
     event.preventDefault();
   });
 
-searchBar.addEventListener("keyup", (e) => {
+searchBarInput.addEventListener("keyup", (e) => {
   e.preventDefault();
   const term = e.target.value.toLowerCase();
-  const messageList = list.getElementsByClassName("ls-msg");
+  const messageList = messages.getElementsByClassName("ls-msg");
   Array.from(messageList).forEach((msgList) => {
     const title = msgList.textContent;
     if (title.toLowerCase().indexOf(e.target.value) !== -1) {
@@ -279,14 +275,17 @@ searchBar.addEventListener("keyup", (e) => {
     }
   });
 });
+
+// Animate send button when clicking
 sendBtn.addEventListener("mousedown", () => {
-  if (input.value || input_file.value) {
+  if (input.value || inputFile.value) {
     sendBtn.innerHTML =
       'Sent &nbsp;<i class="fas fa-chevron-circle-right"></i>';
     sendBtn.style.backgroundColor = "#38b000";
   }
 });
 
+// Animate send button after clicking
 sendBtn.addEventListener("mouseup", () => {
   setTimeout(() => {
     sendBtn.innerHTML = 'Send <i class="fas fa-chevron-circle-right"></i>';
@@ -294,6 +293,7 @@ sendBtn.addEventListener("mouseup", () => {
   }, 400);
 });
 
+// Animate send button after pressing enter key
 input.addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
     sendBtn.click();
@@ -304,11 +304,12 @@ input.addEventListener("keyup", function (event) {
   }
 });
 
-function readThensend() {
+// Sent to server when an image file is uploaded
+function readThenSend() {
   const data = document.querySelector("input[type=file]").files[0];
   const reader = new FileReader();
   reader.onload = function (evt) {
-    var msg = {};
+    let msg = {};
     msg.username = socket.name;
     msg.file = evt.target.result;
     msg.fileName = data.fileName;
@@ -316,31 +317,35 @@ function readThensend() {
   };
   reader.readAsDataURL(data);
 }
-function sendposition(position) {
-  var centerCoordinates = new google.maps.LatLng(37.6, -95.665);
-  var defaultOptions = { center: centerCoordinates, zoom: 4 };
-  var mapLayer = document.createElement("div");
 
-  var map = new google.maps.Map(mapLayer, defaultOptions);
-  var currentLatitude = position.coords.latitude;
-  var currentLongitude = position.coords.longitude;
+// Sent to server current location
+function sendPosition(position) {
+  let centerCoordinates = new google.maps.LatLng(37.6, -95.665);
+  let defaultOptions = { center: centerCoordinates, zoom: 4 };
+  let mapLayer = document.createElement("div");
 
-  var infoWindowHTML =
+  let map = new google.maps.Map(mapLayer, defaultOptions);
+  let currentLatitude = position.coords.latitude;
+  let currentLongitude = position.coords.longitude;
+
+  let infoWindowHTML =
     "Latitude: " + currentLatitude + "<br>Longitude: " + currentLongitude;
-  var infoWindow = new google.maps.InfoWindow({
+  let infoWindow = new google.maps.InfoWindow({
     map: map,
     content: infoWindowHTML,
   });
-  var currentLocation = { lat: currentLatitude, lng: currentLongitude };
+  let currentLocation = { lat: currentLatitude, lng: currentLongitude };
   infoWindow.setPosition(currentLocation);
   socket.emit("location_Send", infoWindow);
 }
-function send_location() {
+
+function sendLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(sendposition);
+    navigator.geolocation.getCurrentPosition(sendPosition);
   }
 }
 
+// Get current time
 function getTime() {
   let date = new Date();
   let hours = date.getHours();
